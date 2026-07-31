fd19e4bb37d307925f258fb5d3ca950a07f3d511
DhivyabharathiCSE <dhivyapillai2005@gmail.com>
2026-07-31 16:06:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = arr.length;
+        
+        // The rightmost element is always a leader
+        int maxFromRight = arr[n - 1];
+        result.add(maxFromRight);
+        
+        // Traverse from right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxFromRight) {
+                maxFromRight = arr[i];
+                result.add(maxFromRight);
+            }
+        }
+        
+        // Reverse to maintain relative order from left to right
+        Collections.reverse(result);
+        
+        return result;
+    }
+}
